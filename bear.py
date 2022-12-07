181a70decc80aef1ef3c04334ac0e689696d6f5c
Pavel-Def <mail.dz.need.to.do@gmail.com>
2022-12-07 20:48:54 +0300

    Initial commit

---
+++ bear.py
@@ -0,0 +1,95 @@
+class animals:
+
+
+
+
+    class bear:
+
+        def __init__(self,name,weightFoodConsumed,age):
+            self.__name=name
+            self.__biome="taiga"
+            self.__eating="рыба","мясо","ягоды"
+            self.__predator =True
+            self.__sound ="yyyeyyeyeeeayyaayayaaaa"
+            self.weightFoodConsumed=weightFoodConsumed
+            self.__age=age
+
+        def doSoud(self):
+            print(self.__sound)
+
+        def eat(self, sfoodType):
+            if (sfoodType in self.__eating):
+                print(self.__name, ": Я покушал", sfoodType)
+            else:
+                print(self.__name, ": Я не буду", sfoodType)
+
+        def play(self):
+            print("*", self.__name, "играет*")
+
+
+        @property
+        def Age(self):
+            return self.__age
+        @property
+        def Biome(self):
+            return self.__biome
+        @property
+        def Eating(self):
+            return self.__eating
+        @property
+        def Predator(self):
+            return self.__predator
+        @property
+        def Sound(self):
+            return self.__sound
+        @property
+        def Name(self):
+            return self.__name
+
+
+
+
+
+    class cat:
+
+        def __init__(self, name, weightFoodConsumed, age):
+            self.__name = name
+            self.__biome = "taiga"
+            self.__eating = "рыба", "мясо", "ягоды"
+            self.__predator = True
+            self.__sound = "yyyeyyeyeeeayyaayayaaaa"
+            self.weightFoodConsumed = weightFoodConsumed
+            self.__age = age
+
+        def doSoud(self):
+            print(self.sound)
+
+        def eat(self):
+            print(self.name, " ест -", self.eating, " весом на ", self.weightFoodConsumed)
+
+        def play(self):
+            print("*", self.name, "играет*")
+
+    class sheep:
+
+        def __init__(self, name, weightFoodConsumed, age):
+            self.__name = name
+            self.__biome = "taiga"
+            self.__eating ="ягоды", "трава"
+            self.__predator = False
+            self.__sound = "yyyeyyeyeeeayyaayayaaaa"
+            self.weightFoodConsumed = weightFoodConsumed
+            self.__age = age
+
+        def doSoud(self):
+            print(self.sound)
+
+        def eat(self):
+            print(self.name, " ест -", self.eating, " весом на ", self.weightFoodConsumed)
+
+        def play(self):
+            print("*", self.name, "играет*")
+
+
+
+
---
+++ bearMeaning.py
@@ -0,0 +1,13 @@
+from bear import *
+bear1=animals.bear(1,2,3)#bear1=cat("name","weightFoodConsumed","age")
+bear1.doSoud()
+bear1.play()
+#bear1.eat()
+print(bear1.Age)
+print(bear1.Biome)
+print(bear1.Eating)
+print(bear1.Predator)
+print(bear1.Sound)
+
+bear1.ReName(32)
+print(bear1.Name)
